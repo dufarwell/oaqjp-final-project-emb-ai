8e32f317b4162539cd21d9693b506d49a4229262
dufarwell <dustinfarwell@yahoo.com>
2024-07-22 20:02:08 -0400

    project3b

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,6 @@
 
 import requests
+import json
 
 URL= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 HEADERS= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
@@ -7,5 +8,17 @@ HEADERS= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stoc
 def emotion_detector(text_to_analyse):
     Inputjson= { "raw_document": { "text": text_to_analyse } }
     pst = requests.post(URL, headers=HEADERS, json=Inputjson)
-    print(pst)
-    return pst
+    rsp = json.loads(pst.text)['emotionPredictions'][0]['emotion']
+    em_rating = 0
+    top_emotion = 'emotionless'
+    
+    for emotion in rsp:
+        if rsp[emotion] > em_rating:
+            em_rating = rsp[emotion]
+            top_emotion = emotion
+
+    rsp['dominant_emotion']=top_emotion
+
+
+
+    return rsp
