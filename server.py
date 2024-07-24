6c0e3961850b448d271465d6a72f35de6d33c719
dufarwell <dustinfarwell@yahoo.com>
2024-07-24 12:28:21 -0400

    6b deployment test

--- server.py
+++ server.py
@@ -1,19 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 app = Flask(__name__)
 
-@app.route('/')
-def test():
-    print('test')
-    return {'message':'test worked'}
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
 
 @app.route('/emotionDetector')
 def emotion_test():
-    text_to_test = request.args.get("text_to_test").replace("%20"," ")
+    
+    text_to_test = request.args.get("textToAnalyze")
     res = emotion_detector(text_to_test)
+    
     rtrn = "For the given statement, the system response is "
     for emotion in [r for r in res if not r == 'dominant_emotion']:
         rtrn += f"'{emotion}': {res[emotion]}, "
     rtrn = rtrn[:-2] + f". The dominant emotion is {res['dominant_emotion']}."
-    print(rtrn)
+
     return rtrn
